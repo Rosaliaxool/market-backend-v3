384251fc4074f3640e5889d17dc1c6c801fcf616
Rosaliaxool <mrrxm150802@gmail.com>
2026-09-08 16:04:49 -0600

    First commit

---
+++ src/main/java/com/merida/tecnm/market_backend_v3/MarketBackendV3Application.java
@@ -0,0 +1,13 @@
+package com.merida.tecnm.market_backend_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBackendV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketBackendV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/merida/tecnm/market_backend_v3/MarketBackendV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merida.tecnm.market_backend_v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBackendV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
